c6bda63dc2343ca17743b16b3030b182e5be5e49
Howell <kelvinuc111@gmail.com>
2025-11-11 15:24:19 +0100

    Initial commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewCompanions = () => {
+  return (
+    <div>
+      NewCompanions
+    </div>
+  )
+}
+
+export default NewCompanions
---
+++ app/subscribtion/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Subscribtion = () => {
+  return (
+    <div>
+      Subscribtion
+    </div>
+  )
+}
+
+export default Subscribtion
