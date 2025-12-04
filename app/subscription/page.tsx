40774b5f0fd0dc76a375dbcc9987025be6b50f8c
Howell <kelvinuc111@gmail.com>
2025-12-04 19:26:06 +0100

    Implemented Auth, Billing, Database Setup, Create Companion Server action

---
+++ app/subscription/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {PricingTable} from "@clerk/nextjs";
+
+const Subscription = () => {
+  return (
+    <div>
+      <PricingTable />
+    </div>
+  )
+}
+
+export default Subscription
